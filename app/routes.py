3005f5e0047548e40268ea86784e2bc97a3f1afc
Laurenz-03 <laurenzruoff2003@gmail.com>
2021-01-31 21:36:34 +0100

    commit

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,14 @@ def login():
 def app_download():
     return render_template('pages/app-download.html', heading="App-Downloads", title="Die GrowMaster App")
     
+@app.route('/coachings')
+def coachings():
+    return render_template('pages/coachings.html', heading="COACHINGS", title="Coachings")
+    
+@app.route('/kurs')
+def kurs_uebersicht():
+    return render_template('pages/kurs.html', heading="GROWMASTER", title="GrowMaster")
+    
 @app.route('/gruppen')
 def gruppen():
     return render_template('pages/gruppen.html', heading="GRUPPEN", title="Gruppen")
@@ -25,3 +33,21 @@ def aff_link():
 @app.route('/growking')
 def growking():
     return render_template('pages/growking.html', heading="GrowKing", title="GrowKing", growkinglogo=True)
+
+@app.route('/bücher')
+def buecher():
+    return render_template('pages/bücher.html', heading="BÜCHER", title="Bücher")
+
+
+#Impressum, AGBs, Datenschutz
+@app.route('/impressum')
+def impressum():
+    return render_template('pages/impressum.html', heading="Impresssum", title="Impresssum", show_top_section=False, sidebar=False)
+
+@app.route('/agb')
+def agb():
+    return render_template('pages/agb.html', heading="AGBs", title="AGBs", show_top_section=False, sidebar=False)
+
+@app.route('/datenschutz')
+def datenschutz():
+    return render_template('pages/datenschutz.html', heading="Datenschutz", title="Datenschutz", show_top_section=False, sidebar=False)
